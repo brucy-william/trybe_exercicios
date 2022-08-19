a02ded074574e798331fc609a43ed618eca67d18
Brucy William <brucywlalmeida@icloud.com>
2022-08-19 01:18:57 -0300

    apanhando menos que nas outras, to com medo

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programação/dia-01-javascript-primeiros-passos/exercicio.js
@@ -0,0 +1,62 @@
+/*const a = 5
+const b = 2
+
+console.log( a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);*/
+
+/*---------------------------------------------------------------*/
+
+/*const c = 10
+const d = 15
+
+if (c > d) {
+    console.log(c);
+} else {
+    console.log(d);
+}*/
+
+/*---------------------------------------------------------------*/
+
+/*const e = 3
+const f = 12
+const g = 10
+let maior = 0;
+let resultado = 0;
+
+if (e > f) { maior = e } 
+else  {maior = f} 
+
+if (maior > g) { resultado = maior }
+else {resultado = g}
+
+console.log(resultado)*/
+
+/*---------------------------------------------------------------*/
+
+/*const parameter = 0;
+
+if (parameter < 0) {
+    console.log("negativo");
+} else if (parameter > 0) {
+    console.log("positivo");
+} else {
+    console.log("0");
+}*/
+
+/*---------------------------------------------------------------*/
+
+/*let internalAngle1 = 50
+let internalAngle2 = 100
+let internalAngle3 = 40
+
+if (internalAngle1 + internalAngle2 + internalAngle3 == 180) {
+    console.log(true)
+} else { console.log(false)
+    
+}*/ 
+
+
+
