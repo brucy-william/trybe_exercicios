0944a4c6c635048f81e1d7d384e75d3334fa0195
Brucy William <brucywlalmeida@icloud.com>
2022-08-24 15:12:50 -0300

    .

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programação/dia-03-javascript-logica-de-programacao-e-algoritimos/ex.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programação/dia-03-javascript-logica-de-programacao-e-algoritimos/ex.js
@@ -1,16 +1,22 @@
-let numbers = [10,9,8,7,6,5,4,3,2,1];
-let resultado = 0;
+// let numbers = [10,9,8,7,6,5,4,3,2,1];
+// let resultado = 0;
 
-// for (let index = 10; index > 0; index -= 1){
-//     numbers.push(index);
-// } 
+// // for (let index = 10; index > 0; index -= 1){
+// //     numbers.push(index);
+// // } 
 
-for (let index = 0; index < numbers.length; index += 1){
-    resultado += numbers[index] * (numbers[index]-1);
+// for (let index = 0; index < numbers.length; index += 1){
+//     resultado += numbers[index] * (numbers[index]-1);
     
-}
+// }
 
-console.log(numbers);
-console.log(resultado);
+// console.log(numbers);
+// console.log(resultado);
+
+let fatorial10 = 0;
+
+fatorial10 = 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1;
+
+console.log(fatorial10);
 
 
